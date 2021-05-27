9dd205a23d8001560cb7a2cb9e18586d7e8984d3
Lexa <amigos.73@mail.ru>
2021-05-27 18:52:38 +0700

    less-2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,66 @@
-const products = [
-    { id: 1, tittle: 'Notebook', price: 2000 },
-    { id: 2, tittle: 'Mouse', price: 400 },
-    { id: 3, tittle: 'Keyboard', price: 300 },
-    { id: 4, tittle: 'Gamepad', price: 205 }
-];
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
 
-const renderProduct = (product, img='https://via.placeholder.com/200x150') => {
-    return `<div class="product-item">
-    <img src="${img}">
-    <h3>${product.tittle}</h3>
-    <p>${product.price}</p>
+  render() {
+    return `<div class="goods-item">
+    <img src='https://via.placeholder.com/200x150'>
+    <h3>${this.title}</h3>
+    <p>${this.price}</p>
     <button class="buy-btn">Купить</button>
     </div>`;
+  }
 }
 
-const renderPage = (list) => {
-    const productList = list.map(item => renderProduct(item));
-    document.querySelector('.products').innerHTML = productList.join('');
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: "Shirt", price: 150 },
+      { title: "Socks", price: 50 },
+      { title: "Jacket", price: 350 },
+      { title: "Shoes", price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = "";
+    this.goods.forEach((good) => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector(".goods-list").innerHTML = listHtml;
+  }
+
+  totalPrice() {
+    let res = this.goods.reduce((acc, curr) => {
+      return { price: acc.price + curr.price };
+    });
+    return res.price;
+  }
 }
 
-renderPage(products);
+class Cart {
+  constructor() {
+    this.goodsList = [];
+  }
+  addItem() {}
+  reoveItem() {}
+  totalPrice() {}
+  order() {}
+}
+
+class CartItem {
+  constructor() {}
+  getInfo() {}
+  incrementCount() {}
+  decrementCount() {}
+}
+
+const prod = new GoodsList();
+prod.fetchGoods();
+prod.render();
+console.log(prod.totalPrice());
